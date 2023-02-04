dfb3c1d044d7221c985f5fd2c6d0de78ca528061
Adam Michell <agmspu@gmail.com>
2023-02-03 20:51:20 -0600

    updated the index.js

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Adam-s-README-generator
+# The BEst Project Ever!
+   
+   ## Description
+   
+   It's the best, what more is there to say?
+   
+   ## Table of Contents
+
+   * [Installation](#Installation) 
+   
+   * [Usage](#Usage)
+   
+   * [Contributing](#Contributing)
+   
+   * [Tests](#Tests)
+   
+   * [Questions](#Questions)
+   
+   ## Installation
+   
+   To install this application run the folllowing command
+   
+   INstall and lettttt's gooooo!
+   
+   ## Usage
+   
+   None that I can think of
+   
+   ## Contibutions
+   
+   Phil from down the street
+   
+   ## Tests
+   
+   We don't need tests where we're going
+   
+   ## Questions
+   
+   Make it twice as fast and half as functional
---
+++ index.js
@@ -0,0 +1,114 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateReadMe = (data) => {
+   return `# ${data.title}
+   
+   ## Description
+   
+   ${data.description}
+   
+   ## Table of Contents
+
+   * [Installation](#Installation) 
+   
+   * [Usage](#Usage)
+   
+   * [Contributing](#Contributing)
+   
+   * [Tests](#Tests)
+   
+   * [Questions](#Questions)
+   
+   ## Installation
+   
+   To install this application run the folllowing command
+   
+   ${data.installation}
+   
+   ## Usage
+   
+   ${data.usage}
+   
+   ## Contibutions
+   
+   ${data.contributing}
+   
+   ## Tests
+   
+   ${data.tests}
+   
+   ## Questions
+   
+   ${data.questions}`
+
+}
+  
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Describe your project',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How is your project installed by users and how do they launch your application?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Are there are specific limitations for usage?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Who would you like to recognize for contributing to this project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What sort of tests are you running for your code?',
+    },
+    {
+      type: 'input',
+      name: 'questions',
+      message: 'Are there questions for future development?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license',
+      choices: ["Alpha", "MIT", "Three"],
+    },
+  ])
+  .then((answers) => {
+    const readMePageContent = generateReadMe(answers);
+
+    fs.writeFileSync('README.md', readMePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
+
+//   GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
